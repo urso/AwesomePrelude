2dbc99df68abfe31623b91dfca96c4c3501d4c87
Steffen <steffen.siering@gmail.com>
2010-02-17 02:20:06 +0100

    Added associated Date type to class Num and commented out all other classes in File Generic/Data/Num

--- src/Generic/Data/Num.hs
+++ src/Generic/Data/Num.hs
@@ -9,55 +9,50 @@ import qualified Prelude
 infixl 6 +
 infixl 7 *
 infixl 6 -
-infixl 7 /
+-- infixl 7 /
 
 class Num l a where
-  negate :: l a -> l a
-  abs    :: l a -> l a
-  signum :: l a -> l a
-  fromInteger :: Prelude.Integer -> l a
-  (+) :: l a -> l a -> l a 
-  (-) :: l a -> l a -> l a
-  (*) :: l a -> l a -> l a
+  data TNum l a :: *
+  negate :: l (TNum l a) -> l (TNum l a)
+  abs    :: l (TNum l a) -> l (TNum l a)
+  signum :: l (TNum l a) -> l (TNum l a)
+  fromInteger :: Prelude.Integer -> l (TNum l a)
+  (+) :: l (TNum l a) -> l (TNum l a) -> l (TNum l a)
+  (-) :: l (TNum l a) -> l (TNum l a) -> l (TNum l a)
+  (*) :: l (TNum l a) -> l (TNum l a) -> l (TNum l a)
 
-class Integral l a where
-  quot    :: l a -> l a -> l a
-  rem     :: l a -> l a -> l a
-  div     :: l a -> l a -> l a
-  mod     :: l a -> l a -> l a
-  quotRem :: l a -> l a -> l (a, a)
-  divMod  :: l a -> l a -> l (a, a)
-
-class FractionalOp l a where
-  recip :: a -> a
-
-class (FractionalOp l a, FractionalOp l b) => Fractional l a b where
-  type TFractionalResult a b
-  (/) :: l a -> l b -> l (TFractionalResult a b)
-
-class Floating l a where
-  pi    :: l a
-  exp   :: l a
-  sqrt  :: l a
-  log   :: l a
-  sin   :: l a
-  cos   :: l a
-  tan   :: l a
-  asin  :: l a
-  acos  :: l a
-  atan  :: l a
-  sinh  :: l a
-  cosh  :: l a
-  tanh  :: l a
-  asinh :: l a
-  acosh :: l a
-  atanh :: l a
-
--- class Num j a where
---   (+)         :: j a -> j a -> j a
---   (-)         :: j a -> j a -> j a
---   (*)         :: j a -> j a -> j a
---   fromInteger :: Prelude.Integer -> j a
+-- class Integral l a where
+--   quot    :: l a -> l a -> l a
+--   rem     :: l a -> l a -> l a
+--   div     :: l a -> l a -> l a
+--   mod     :: l a -> l a -> l a
+--   quotRem :: l a -> l a -> l (a, a)
+--   divMod  :: l a -> l a -> l (a, a)
+-- 
+-- class FractionalOp l a where
+--   recip :: a -> a
+-- 
+-- class (FractionalOp l a, FractionalOp l b) => Fractional l a b where
+--   type TFractionalResult a b
+--   (/) :: l a -> l b -> l (TFractionalResult a b)
+-- 
+-- class Floating l a where
+--   pi    :: l a
+--   exp   :: l a
+--   sqrt  :: l a
+--   log   :: l a
+--   sin   :: l a
+--   cos   :: l a
+--   tan   :: l a
+--   asin  :: l a
+--   acos  :: l a
+--   atan  :: l a
+--   sinh  :: l a
+--   cosh  :: l a
+--   tanh  :: l a
+--   asinh :: l a
+--   acosh :: l a
+--   atanh :: l a
 
 -- Terrible hack to get number literals working.
 
