a1c0b754e90c1e7b2f51066b9d19cbcd76c809da
Steffen <steffen.siering@gmail.com>
2010-02-17 22:28:40 +0100

    removed NameC context from FunC class + added associated type to FunC class

--- src/Generic/Control/Function.hs
+++ src/Generic/Control/Function.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE FlexibleInstances, MultiParamTypeClasses #-}
 
 module Generic.Control.Function where
@@ -11,14 +12,18 @@ undefined = Prelude.undefined
 class NameC j where
   named :: Prelude.String -> j a -> j a
 
-class NameC j => FunC j where
-  lam :: (j a -> j b) -> j (a -> b)
-  app :: j (a -> b) -> j a -> j b
+class FunC j where
+  type TFun j :: * -> * -> *
+  lam :: (j a -> j b) -> j (TFun j a b)
+  app :: j (TFun j a b) -> j a -> j b
+  -- lam :: (j a -> j b) -> j (a -> b)
+  -- app :: j (a -> b) -> j a -> j b
 
-class FunC j => RecFunC j where
-  fix :: (j (a -> b) -> j (a -> b)) -> j (a -> b)
+class FunC l => RecFunC l where
+--   fix :: (l (a -> b) -> l (a -> b)) -> l (a -> b)
+  fix :: (l (TFun l a b) -> l (TFun l a b)) -> l (TFun l a b)
 
-instance FunC j => Category j (->) where
+instance (NameC j, FunC j) => Category j (->) where
   id a      = "id" `named` (lam (\i -> i) `app` a)
   (.) f g a = lam f `app` (lam g `app` a)
 
@@ -30,17 +35,19 @@ infixr 0 $
 const :: FunC j => j a -> j b -> j a
 const a b = lam2 (\c _ -> c) `app` a `app` b
 
--- Helper functions.
+-- -- Helper functions.
 
-lam2 :: FunC j => (j a -> j b -> j c) -> j (a -> b -> c)
+lam2 :: FunC j => (j a -> j b -> j c) -> j (TFun j a (TFun j b c))
 lam2 f = lam (\a -> lam (f a))
 
-lam3 :: FunC j => (j a -> j b -> j c -> j d) -> j (a -> b -> c -> d)
+lam3 :: (FunC j) => (j a -> j b -> j c -> j d) -> j (TFun j a (TFun j b (TFun j c d)))
 lam3 f = lam (\a -> lam2 (f a))
 
-app2 :: FunC j => j (a -> b -> c) -> j a -> j b -> j c
+-- app2 :: FunC j => j (a -> b -> c) -> j a -> j b -> j c
+app2 :: (FunC j) => j (TFun j a (TFun j b c)) -> j a -> j b -> j c
 app2 f x y = (f `app` x) `app` y
 
-app3 :: FunC j => j (a -> b -> c -> d) -> j a -> j b -> j c -> j d
+-- app3 :: FunC j => j (a -> b -> c -> d) -> j a -> j b -> j c -> j d
+app3 :: (FunC j) => j (TFun j a (TFun j b (TFun j c d))) -> j a -> j b -> j c -> j d
 app3 f x y z = app2 f x y `app` z
 
