80d78da43f38b09d73160dfa14db42fa91cc37ca
Steffen <steffen.siering@gmail.com>
2010-02-17 02:25:00 +0100

    renamed class Ordering to OrderingC

--- src/Generic/Data/Ord.hs
+++ src/Generic/Data/Ord.hs
@@ -10,17 +10,16 @@ import qualified Prelude as P
 import Generic.Data.Bool
 import Generic.Data.Eq
 
--- infix  4  <, <=, >=, >
+infix  4  <, <=, >=, >
 
-
-class Ordering l where
+class OrderingC l where
   data TOrdering l
   lt :: l (TOrdering l)
   gt :: l (TOrdering l)
   eq :: l (TOrdering l)
   ordering :: l a -> l a -> l a -> l (TOrdering l) -> l a
 
-class (Eq l a, Ordering l) => OrdC l a where
+class (Eq l a, OrderingC l) => OrdC l a where
   compare :: l a -> l a -> l (TOrdering l)
   (<), (<=), (>), (>=) :: l a -> l a -> l (TBool l)
   max, min             :: l a -> l a -> l a
@@ -35,22 +34,22 @@ class (Eq l a, Ordering l) => OrdC l a where
   max x y = if' (x <= y) y x
   min x y = if' (x <= y) x y
 
-instance (Ordering l, BoolC l) => OrdC l (TBool l) where
+instance (OrderingC l, BoolC l) => OrdC l (TBool l) where
   x <= y = if' x true (if' y false true)
 
-instance (Ordering l, BoolC l) => Eq l (TOrdering l) where
+instance (OrderingC l, BoolC l) => Eq l (TOrdering l) where
   x == y = ordering (ordering true  false false y)
                     (ordering false true  false y)
                     (ordering false false true  y)
                     x
 
-instance (Ordering l, BoolC l) => OrdC l (TOrdering l) where
+instance (OrderingC l, BoolC l) => OrdC l (TOrdering l) where
   x <= y = ordering true -- (ordering true  true  true y)
                     (ordering false true  true y)
                     (ordering false false true y)
                     x
 
-comparing :: (OrdC l a, BoolC l, Ordering l) => 
+comparing :: (OrdC l a, BoolC l, OrderingC l) => 
              (b -> l a) -> b -> b -> l (TOrdering l)
 comparing p x y = compare (p x) (p y)
 
