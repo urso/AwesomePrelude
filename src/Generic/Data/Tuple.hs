fb23f489a67b77daaf2930128f0126d822e8b2e3
Steffen <steffen.siering@gmail.com>
2010-02-17 02:25:32 +0100

    added associated type to class TupleC

--- src/Generic/Data/Tuple.hs
+++ src/Generic/Data/Tuple.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE FlexibleInstances, MultiParamTypeClasses #-}
 
 module Generic.Data.Tuple where
@@ -8,15 +9,17 @@ import Generic.Data.Eq
 import Generic.Data.Ord
 
 class TupleC j where
-  mkTuple :: j a -> j b -> j (a, b) 
-  tuple   :: (j a -> j b -> j r) -> j (a, b) -> j r
+  data TTuple j :: * -> * -> *
 
-instance (BoolC j, TupleC j, Eq j a, Eq j b) => Eq j (a, b) where
+  mkTuple :: j a -> j b -> j (TTuple j a b)
+  tuple   :: (j a -> j b -> j r) -> j (TTuple j a b) -> j r
+
+instance (BoolC j, TupleC j, Eq j a, Eq j b) => Eq j (TTuple j a b) where
   x == y = tuple (\xa xb -> tuple (\ya yb -> xa == ya && xb == yb) y) x
 
-instance (BoolC j, TupleC j, Eq j a, Eq j b, OrdC j a, OrdC j b) => OrdC j (a, b) where
+instance (BoolC j, TupleC j, Eq j a, Eq j b, OrdC j a, OrdC j b) => OrdC j (TTuple j a b) where
   x <= y = tuple (\xa xb -> tuple (\ya yb -> xa <= ya && xb <= yb) y) x
 
-swap :: TupleC j => j (a, b) -> j (b, a)
+swap :: TupleC j => j (TTuple j a b) -> j (TTuple j b a)
 swap = tuple (\a b -> mkTuple b a)
 
